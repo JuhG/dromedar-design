d6e2c10fb26ae7bdb95b2a074d87353f1f5570ca
Gabor Juhasz <gabor.juhasz@accretivetg.com>
2021-12-21 19:15:09 +0100

    feat: add content

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,7 +1,32 @@
 import type { NextPage } from "next";
+import Logo from "../assets/logo.svg";
+import Image from "next/image";
 
 const Home: NextPage = () => {
-  return <h1>hello</h1>;
+  return (
+    <main className="bg min-h-full p-[3vw] flex flex-col">
+      <header className="flex flex-col lg+:flex-row-reverse justify-between">
+        <div className="max-w-[50%]">
+          <Image width="200" height="200" src={Logo} alt="Dromedar Design logo" />
+        </div>
+        <h1 className="h-[38vw] w-[94vw] text-[18vw] leading-[18vw] lg:h-[380px] lg:w-[1000px] lg:text-[180px] lg:leading-[180px] from-dd-purple/75 via-dd-red/75 to-dd-purple/75 bg-gradient-to-r font-extrabold text-transparent bg-clip-text">
+          Dromedar Design
+        </h1>
+      </header>
+      <p className="mt-[3vw] w-[94vw] lg:w-[1000px] !leading-[1.3] text-3xl sm:text-5xl md:text-6xl font-serif font-semibold text-dd-black">
+        Online megoldások <br /> modern cégeknek
+      </p>
+      <span className="flex-1"></span>
+      <footer className="my-[3vw]">
+        <a
+          className="w-[94vw] lg:w-[1000px] !leading-[1.3] text-2xl sm:text-4xl md:text-5xl font-serif font-semibold text-dd-red"
+          href="mailto:info@dromedar.design"
+        >
+          info@dromedar.design
+        </a>
+      </footer>
+    </main>
+  );
 };
 
 export default Home;
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,10 +1,19 @@
 module.exports = {
-  content: [
-    "./pages/**/*.{js,ts,jsx,tsx}",
-    "./components/**/*.{js,ts,jsx,tsx}",
-  ],
+  content: ["./pages/**/*.{js,ts,jsx,tsx}", "./components/**/*.{js,ts,jsx,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      screens: {
+        "lg+": "1100px",
+      },
+      colors: {
+        dd: {
+          teal: "#04ABD5",
+          black: "#242539",
+          purple: "#9800FF",
+          red: "#EF4E6E",
+        },
+      },
+    },
   },
   plugins: [],
 };
